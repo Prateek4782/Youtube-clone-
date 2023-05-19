8e4359749f07bcf52c1476b8faf1c0a438e7f8a0
Prateek4782 <106441817+Prateek4782@users.noreply.github.com>
2023-05-19 20:30:09 +0530

    v1 youtube project

--- app.js
+++ app.js
@@ -6,3 +6,17 @@ menuIcon.onclick = function(){
     sidebar.classList.toggle("small-sidebar");
     container.classList.toggle("large-container")
 }
+
+
+// --------------dark-theme----------------
+
+icon.addEventListener('click', function() {
+    document.body.classList.toggle("white-theme");
+    if(document.body.classList.contains('white-theme')){
+        icon.src = "images/moon.png"
+    }else{
+        icon.src = "images/sun.png"
+    }
+})
+
+
